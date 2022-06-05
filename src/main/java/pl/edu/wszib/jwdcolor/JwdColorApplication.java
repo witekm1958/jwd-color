a5279227d4eda7d0507d89caba97c61ae0f76ace
matusins <matusins@KOMP409-11>
2022-06-05 09:26:48 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwdcolor/JwdColorApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdcolor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdColorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwdColorApplication.class, args);
+    }
+
+}
